2827c9d26512a68b9597839be8875b360519c395
mi379 <mkzbdgfh@gmail.com>
2022-09-02 23:31:21 +0700

    fixing deploy error

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
-import postcss from './postcss.config.cjs';
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  css:{
-    postcss
-  }
 })
